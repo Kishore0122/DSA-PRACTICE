40c78585d99b69af2e5c2cb3ec9ce6131f02a55a
kishore0122 <kishorepanuganti278@gmail.com>
2026-05-24 15:26:02 +0530

    DSA PRACTICE

--- Medium/Search_a_2D_Matrix.js
+++ Medium/Search_a_2D_Matrix.js
@@ -1,20 +1,18 @@
 let matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
 let target=3
 let isfound=false
-// for(let e of matrix){
-//     for(let c of e){
-//         if(c===target){
-//             isfound=true
-//         }
-// }
-// }
+for(let e of matrix){
+    for(let c of e){
+        if(c===target){
+            isfound=true
+        }
+}
+}
 
 // let arr=[].concat(...matrix)
 // for(let i=0;i<arr.length;i++){
 //     if(i===target) isfound=true
 // }
-// console.log(isfound)
+console.log(isfound)
 
-let left=0
-let right=
 
